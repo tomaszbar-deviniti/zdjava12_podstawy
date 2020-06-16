d25ad964e8f46eeccda5c0e8966df3cf107cdcd6
tomaszbar-deviniti <62062074+tomaszbar-deviniti@users.noreply.github.com>
2020-06-16 22:47:06 +0200

    Update DecimalToBinary.java
    wysłałem ci kod na slacku mojego przykładowego rozwiązania

--- src/rozwiazaniazadan/DecimalToBinary.java
+++ src/rozwiazaniazadan/DecimalToBinary.java
@@ -12,11 +12,11 @@ public class DecimalToBinary {
         while(Math.abs(number) >= Math.pow(2,i)){
             i++;
         }
-        int[] binaryRepresentation = new int[i+1];
+        int[] binaryRepresentation = new int[i+1]; //możnaby pominąć tą pośrednią tablicę
         if(number < 0){
             binaryRepresentation[0] = 1;
         }
-        number = Math.abs(number);
+        number = Math.abs(number); //raczej powinniśmy wyjść z metody, zamiast podstawiać za niego wartość absolutną w przypadku liczby ujemnej
         for(int j=--i;number>0;j--){
             if(Math.pow(2,j) <= number){
                 binaryRepresentation[binaryRepresentation.length-j-1] = 1;
